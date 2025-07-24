f06f2de4fcc013046289990396d683e119fcdb88
hanwix2 <alexsjh23@gmail.com>
2025-07-24 22:45:33 +0900

    Feature: 쿠폰 발급 요청 값 수정

--- src/main/kotlin/kr/hhplus/be/server/presentation/request/CouponIssueRequest.kt
+++ src/main/kotlin/kr/hhplus/be/server/presentation/request/CouponIssueRequest.kt
@@ -4,6 +4,10 @@ import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(description = "쿠폰 발급 요청")
 data class CouponIssueRequest(
+
+    @Schema(description = "유저 ID", example = "1")
+    val userId: Long,
+
     @Schema(description = "발급받을 쿠폰 ID", example = "12345")
     val couponId: Long
 )
