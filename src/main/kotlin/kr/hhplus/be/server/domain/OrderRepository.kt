b9e50fb5fe4df6f83155caf7634ccf65f39a8566
hanwix2 <alexsjh23@gmail.com>
2025-08-01 05:19:58 +0900

    Test: OrderService 통합 테스트 추가

--- src/main/kotlin/kr/hhplus/be/server/domain/OrderRepository.kt
+++ src/main/kotlin/kr/hhplus/be/server/domain/OrderRepository.kt
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface OrderRepository : JpaRepository<Order, Long>
+interface OrderRepository : JpaRepository<Order, Long> {
+    fun findByUserId(userId: Long): List<Order>
+}
